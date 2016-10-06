728fdb40dc827e5f1ba6d7f608e8d65d1a4876fb
xiaoyue26 <296671657@qq.com>
2016-10-06 19:33:51 +0800

    update controller

--- src/main/java/com/xiaoyue26/www/controller/TokenController.java
+++ src/main/java/com/xiaoyue26/www/controller/TokenController.java
@@ -1,7 +1,16 @@
 package com.xiaoyue26.www.controller;
 
+import com.xiaoyue26.www.entity.Check;
+import com.xiaoyue26.www.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 /**
  * Created by xiaoyue26 on 10/6/16.
@@ -9,4 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/wechat")
 public class TokenController {
+    @Autowired
+    private TokenService tokenService;
+
+
+    @RequestMapping(value = "/check/{wxToken}", method = RequestMethod.GET, produces = "text/plain")
+    public
+    @ResponseBody
+    String validate(@PathVariable("wxToken") String wxToken, Check tokenModel) throws ParseException, IOException {
+        return tokenService.validate(wxToken, tokenModel);
+    }
 }
